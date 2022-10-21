35de590c0936ef5215d8582130a865d63036ce0e
Léo Weverton <leoweverton@MacBook-Air-de-Leo.local>
2022-10-20 23:35:27 -0300

    project(netflix-clone): clonar algumas paginas e fluxo do netflix

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBUufvFC5VkU-DHMRDylhDe32Y_ltF9mQI",
+  apiKey: "",
   authDomain: "netflix-clone-b6f5e.firebaseapp.com",
   projectId: "netflix-clone-b6f5e",
   storageBucket: "netflix-clone-b6f5e.appspot.com",
-  messagingSenderId: "552064505912",
-  appId: "1:552064505912:web:f4c312947f17ae15bfaecd",
+  messagingSenderId: "",
+  appId: "",
 };
 
 const app = initializeApp(firebaseConfig);
